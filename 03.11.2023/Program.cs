3c3b1193e3fd8b5f20bb03f5c73224bc0b36586f
Who1s1t <96753821+Who1s1t@users.noreply.github.com>
2023-11-02 19:23:24 +0300

    Add files via upload

---
+++ 03.11.2023/Program.cs
@@ -0,0 +1,39 @@
+﻿// 2 задание
+//Console.WriteLine("Введите x");
+//double x = Convert.ToDouble(Console.ReadLine());
+//Console.WriteLine("Введите y");
+//double y = Convert.ToDouble(Console.ReadLine());
+//if (x*x + y * y >= 9 && x*x + y*y <= 25 && x >= 0)
+//{
+//    Console.WriteLine("Находится");
+//}
+//else
+//{
+//    Console.WriteLine("НЕ находится");
+//}
+// 3 задание
+//Console.WriteLine("Введите x");
+//double x = Convert.ToDouble(Console.ReadLine());
+//Console.WriteLine("Введите y");
+//double y = Convert.ToDouble(Console.ReadLine());
+//if (y<=4 && y >= 0&& x >= -2 && x <=4 || y >= -3 && y <= 0 && x >= -4 && x <= 5)
+//{
+//    Console.WriteLine("Находится");
+//}
+//else
+//{
+//    Console.WriteLine("НЕ находится");
+//}
+// 4 задание
+//Console.WriteLine("Введите x");
+//double x = Convert.ToDouble(Console.ReadLine());
+//Console.WriteLine("Введите y");
+//double y = Convert.ToDouble(Console.ReadLine());
+//if (y >= 5 - x && y >= x -3 && y <= 3 + x && y <= -x + 11)
+//{
+//    Console.WriteLine("Находится");
+//}
+//else
+//{
+//    Console.WriteLine("НЕ находится");
+//}
